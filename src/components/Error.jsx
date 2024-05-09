eb57e6b3289c15dd8f31448c58dfe5e29361a5b9
RandysHub <106208354+RandysHub@users.noreply.github.com>
2024-05-09 02:07:35 -0400

    Handling fetching error

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error({ message, title }) {
+  return (
+    <div className='error'>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  )
+}
